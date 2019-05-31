3a1361e8a46b6bbef0772d8fa9aa8cfd2406ead9
Pedro Kress <phkress@gmail.com>
2019-05-31 12:54:46 -0300

    Configuração das rotas

--- vue-cli/src/routes.js
+++ vue-cli/src/routes.js
@@ -1,1 +1,4 @@
-export const routes = [];
+import Home from './components/home/Home.vue';
+export const routes = [
+  {path: '*', name:'homeGeral', component: Home, titulo: 'Home', menu: false},
+];
