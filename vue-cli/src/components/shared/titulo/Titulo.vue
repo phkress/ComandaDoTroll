238c5cff616c3e4f4d7733ae5fc5f61a3bec08e2
Kress <phkress@globo.com>
2019-06-04 20:07:55 -0300

    Update Titulo.vue

--- vue-cli/src/components/shared/titulo/Titulo.vue
+++ vue-cli/src/components/shared/titulo/Titulo.vue
@@ -1,12 +1,32 @@
 <template>
-    
+    <div id="Titulo">
+        <b-container fluid>
+            <b-row class="text-center mt-3">
+                <b-col> </b-col>
+                <b-col cols="8">
+                     <h1>{{titulo}}</h1>
+                </b-col>
+                <b-col>
+                    RELOGIO
+                </b-col>
+            </b-row>
+        </b-container>   
+    </div>
 </template>
 <script>
 export default {
     name: "Titulo",
-    data: () => ({
+    props:{
+        titulo:{
+            required: true,
+            type: String            
+        }
+    },
+    data() {
+        return {
 
-    }),
+        }
+    },
 }
 </script>
 <style scoped>
