1ed8ac17c351bd449e0f4a94ef22e392aaab62df
Kress <phkress@globo.com>
2019-06-04 20:07:51 -0300

    Update Cozinha.vue

--- vue-cli/src/components/cozinha/Cozinha.vue
+++ vue-cli/src/components/cozinha/Cozinha.vue
@@ -1,9 +1,15 @@
 <template>
-    
+    <div id="Cozinha">
+        <my-titulo titulo="Pedidos"></my-titulo>
+    </div>
 </template>
 <script>
+import Titulo from '../shared/titulo/Titulo.vue';
 export default {
     name: "Cozinha",
+    components: {
+    'my-titulo' : Titulo,
+  },
     data: () => ({
 
     }),
