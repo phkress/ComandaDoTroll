f80aa689855b44d54423f7ade3f676fe0b1ebb25
Pedro Kress <phkress@gmail.com>
2019-06-12 16:25:33 -0300

    Serviço de configuração

--- vue-cli/src/domain/config/Config.js
+++ vue-cli/src/domain/config/Config.js
@@ -1,6 +1,12 @@
 export default class Config {
 
   constructor(){
-
+    this.numeroMesas
+  }
+  getNumeroMesas() {
+    return this.numeroMesas
+  }
+  setNumeroMesas(valor){
+    this.numeroMesas = valor
   }
 }
--- vue-cli/src/services/ConfigService.js
+++ vue-cli/src/services/ConfigService.js
@@ -4,4 +4,25 @@ export default class ConfigService {
   constructor(resource){
     this._resource = resource('config');
   }
+  lista(){
+    return this._resource
+    .query()
+    .then(res => res.json(), err=>{
+      console.log(err);
+      throw new Error('Não foi possivel obter a lista de Config')
+    });
+  }
+  cadastra(config) {
+      if(config._id) {
+
+          return this._resource
+              .update({ id: config._id}, config);
+
+      } else {
+          return this._resource
+              .save(config);
+      }
+
+  }
+
 }
