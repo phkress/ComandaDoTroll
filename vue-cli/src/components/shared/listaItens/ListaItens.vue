651d45b1ab3c898de2ade007c718dfa0d3911ce1
Pedro Kress <phkress@gmail.com>
2019-09-03 22:04:04 -0300

    Criação da lista de itens a ser apresentada para o operador

---
+++ vue-cli/src/components/shared/listaItens/ListaItens.vue
@@ -0,0 +1,44 @@
+<template>
+  <div id="listaMesas">
+    <b-row>
+     <b-col cols="12" class="mx-auto">
+         <div class="mesaLista borda" v-for="(iten,index) in itens" v-on:click="comunicaIten(_id, nome)">
+               <span class="float-left">
+                 Item: {{iten.nome}}
+               </span>
+         </div>
+     </b-col>
+    </b-row>
+  </div>
+</template>
+<script>
+export default {
+  name: "listaMesas",
+  props:{
+    itens:{
+      type:Array
+    }
+  },
+  data: () => ({
+
+  }),
+  methods: {
+    comunicaIten(_id, nomex) {
+      this.$emit("itenSelecionado",  {_id, nome} );
+    },
+  }
+}
+</script>
+<style lang="scss" scoped>
+.borda{
+  border:thin solid black;
+  border-top: none;
+}
+.mesaLista{
+  /* text-align: left; */
+  padding-top: 1em;
+  padding-left: 0.7em;
+  padding-right: 0.7em;
+  height: 3.5em;
+}
+</style>
